72860df61b48682a181a690eaf7d6cbe27791088
Bingling Dong <bingling.dong@stonybrook.edu>
2019-12-04 16:40:46 -0500

    made changes to the style and layout and content of the site.

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'https://raw.githubusercontent.com/binglingdong/Resume/gh-pages/resumeData.json',
+      url: "resumeData.json",
+      //url:'https://raw.githubusercontent.com/binglingdong/Resume/gh-pages/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Template by <a title="Styleshout" href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>
            </ul>
 
         </div>
